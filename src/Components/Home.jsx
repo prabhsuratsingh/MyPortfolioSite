6a633be2764c6fe07746b1acc2e9c7a71c3d3016
Prabhsurat Singh <144260379+prabhsuratsingh@users.noreply.github.com>
2024-02-24 16:49:57 +0000

    minor pic change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/ATR_4736.jpg";
+import image from "../images/mypic.jpg";
 
 const imageAltText = "Just Me";
 
